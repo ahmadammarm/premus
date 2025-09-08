b51ef5d8bbcda0e8a53dbbfe92ddc41cf5ed3042
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-09-08 19:11:54 +0700

    refactor: adjust payment notification controller

--- src/routers/payment/paymentroutes.ts
+++ src/routers/payment/paymentroutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { PaymentNotificationController, PaymentController } from "../../controllers/payment/payment";
+import { PaymentNotifcationController, PaymentController } from "../../controllers/payment/payment";
 import isAuthenticated from "../../middlewares/jwt";
 
 
 const paymentrouter = Router();
 
 paymentrouter.post('/create-payment', isAuthenticated, PaymentController);
-paymentrouter.post('/payment-notification', PaymentNotificationController);
+paymentrouter.post('/payment-notification', PaymentNotifcationController);
 
 export default paymentrouter;
